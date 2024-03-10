421c95a34d1f911ac443bb861a14b59e7df573c5
Georgi <georgi.chichev@pytek.bg>
2024-03-10 23:58:51 +0200

    Add homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
 export default function Page() {
-    return <main>Hello</main>;
+  return (
+    <main className="min-h-screen p-6 bg-foreground flex flex-col items-center sm:items-start">
+      <div className="flex flex-col">
+        <h1 className="scroll-m-20 text-5xl font-extrabold tracking-tight lg:text-6xl mt-36 md:mt-96">
+          Prahosneek.
+        </h1>
+        <p className="scroll-m-20 text-1xl font-semibold tracking-tight lg:text-2xl">
+          Do not prahosvay your money
+        </p>
+        <Button asChild size="lg" className="mt-6">
+          <Link href="/login">Login</Link>
+        </Button>
+      </div>
+    </main>
+  );
 }
